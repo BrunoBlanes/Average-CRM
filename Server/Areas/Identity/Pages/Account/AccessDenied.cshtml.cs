92678af119d42cb5ebb4cd391dc82c27a57913b1
Bruno Blanes <bruno.blanes@outlook.com>
2020-07-11 14:29:36 -0300

    Testing GPG signed commits.

--- Server/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CRM.Server.Areas.Identity.Pages.Account
 {
 	public class AccessDeniedModel : PageModel
 	{
-		public void OnGet()
+		public static void OnGet()
 		{
 
 		}
 	}
 }
-
