5a010a057b052bb872450183c091ec28b54ae36f
Bruno Blanes <bruno.blanes@outlook.com>
2020-08-02 03:32:21 -0300

    Added Microsoft.TypeScript.MSBuild package reference and Webpack. Resolves #41

---
+++ Core/Libraries/src/index.tsx
@@ -0,0 +1,10 @@
+﻿import { Buttons } from './buttons';
+import { Functions } from './functions';
+
+export function CreateButton(primary: boolean, content: string, parent: string): void {
+	Buttons.CreateButton(primary, content, parent);
+}
+
+export function StopScroll(id: string): void {
+	Functions.StopScroll(id);
+}
---
+++ Core/Libraries/webpack.config.js
@@ -0,0 +1,42 @@
+﻿const path = require("path");
+
+module.exports = [
+	{
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					}
+				}
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, "../../Client/wwwroot/js"),
+			filename: "jsInterop.js",
+			library: "JsFunctions",
+			libraryTarget: "window"
+		}
+	},
+	{
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					}
+				}
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, "../../Server/wwwroot/js"),
+			filename: "jsInterop.js",
+			library: "JsFunctions",
+			libraryTarget: "window"
+		}
+	}
+];
