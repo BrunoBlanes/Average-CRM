f06601a340f73e214cbd2524764a39848055fd22
Bruno Blanes <bruno.blanes@outlook.com>
2020-10-18 12:24:34 -0300

    Removed autogenerated JavaScript files

--- Core/Libraries/src/index.js
+++
@@ -1,9 +0,0 @@
-import { Buttons } from './buttons';
-import { Functions } from './functions';
-export function CreateButton(primary, content, parent) {
-    Buttons.CreateButton(primary, content, parent);
-}
-export function StopScroll(id) {
-    Functions.StopScroll(id);
-}
-//# sourceMappingURL=index.js.map
