36a13f799f1d743c361ed4cb416781f84876329a
Bruno Blanes <bruno.blanes@outlook.com>
2020-10-24 16:28:14 -0300

    Exclude webpack config from gitignore

---
+++ Core/webpack.config.js
@@ -0,0 +1,24 @@
+﻿const path = require("path");
+
+module.exports = [
+	{
+		entry: "./Scripts/Index.js",
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					}
+				}
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, "../Server/wwwroot/script"),
+			filename: "site.min.js",
+			library: "JsFunctions",
+			libraryTarget: "window"
+		}
+	}
+];
