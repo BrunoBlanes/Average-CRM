096d8ce6cf51072e05e6675e0c56bc8f701e5142
Bruno Blanes <bruno.blanes@outlook.com>
2020-10-27 20:28:11 -0300

    Encrypt the email password for storage

---
+++ Server/Controllers/EmailSettingsController.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+
+using CRM.Core.Models;
+using CRM.Server.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Server.Controllers
+{
+	[ApiController]
+	[ApiVersion("1.0")]
+	[Route("api/[controller]")]
+	public class EmailSettingsController : Controller
+	{
+		private readonly ApplicationDbContext context;
+
+		public EmailSettingsController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<EmailSettings>> OnGetAsync()
+		{
+			return await context.EmailSettings.SingleOrDefaultAsync() is EmailSettings settings
+				? settings
+				: (ActionResult<EmailSettings>)NoContent();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> OnPostAsync([FromForm] EmailSettings settings)
+		{
+			if (settings.Address is null)
+			{
+				settings.Address = settings.Login;
+			}
+
+			if (settings.Name is null)
+			{
+				settings.Name = "CRM Server";
+			}
+
+			using var passwordHasher = new PasswordHasher();
+			settings.Password = passwordHasher.Encrypt(settings.Password, settings);
+
+			if (await context.EmailSettings.SingleOrDefaultAsync() is EmailSettings oldSettings)
+			{
+				oldSettings.Name = settings.Name;
+				context.EmailSettings.Update(oldSettings);
+			}
+
+			else
+			{
+				context.EmailSettings.Add(settings);
+			}
+
+			await context.SaveChangesAsync();
+			return RedirectToPage("~/");
+		}
+	}
+}
--- Server/Services/EmailService.cs
+++ Server/Services/EmailService.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 using CRM.Core.Models;
+using CRM.Server.Data;
 
 using MailKit.Net.Smtp;
 
 using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.EntityFrameworkCore;
 
 using MimeKit;
 
@@ -13,26 +16,41 @@ namespace CRM.Server.Services
 {
 	public class EmailService : IEmailSender
 	{
-		private readonly ApplicationDbContext context;
+		private readonly IHttpClientFactory clientFactory;
 
-		public EmailService(ApplicationDbContext context)
+		public EmailService(IHttpClientFactory clientFactory)
 		{
-			this.context = context;
+			this.clientFactory = clientFactory;
 		}
 
 		public async Task SendEmailAsync(string email, string subject, string body)
 		{
-			Settings settings = await context.Settings.Include(x => x.EmailSettings).FirstOrDefaultAsync();
-			var message = new MimeMessage();
-			message.From.Add(new MailboxAddress(settings.EmailSettings.Name, settings.EmailSettings.Address));
-			message.To.Add(MailboxAddress.Parse(email));
-			message.Subject = subject;
-			message.Body = new TextPart("plain") { Text = $@"{body}" };
-			using var client = new SmtpClient();
-			await client.ConnectAsync(settings.EmailSettings.Server, settings.EmailSettings.Port, settings.EmailSettings.SecureSocketOptions);
-			await client.AuthenticateAsync(settings.EmailSettings.Login, settings.EmailSettings.Password);
-			await client.SendAsync(message);
-			await client.DisconnectAsync(true);
+			// Gets the settings from the database
+			HttpClient httpClient = clientFactory.CreateClient("ServerAPI");
+			HttpResponseMessage? response = await httpClient.GetAsync("EmailSettings");
+
+			if (response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.NoContent)
+			{
+				EmailSettings settings = await response.Content.ReadFromJsonAsync<EmailSettings>() ?? new();
+				// Decrypts the password
+				string password = settings.Password;
+				using var passwordHasher = new PasswordHasher();
+				password = passwordHasher.Decrypt(password, settings);
+
+				// Create the email message
+				var message = new MimeMessage();
+				message.From.Add(new MailboxAddress(settings.Name, settings.Address));
+				message.To.Add(MailboxAddress.Parse(email));
+				message.Subject = subject;
+				message.Body = new TextPart("plain") { Text = $@"{body}" };
+
+				// Connect to the server and send the message
+				using var smtpClient = new SmtpClient();
+				await smtpClient.ConnectAsync(settings.Server, settings.Port, settings.SecureSocketOptions);
+				await smtpClient.AuthenticateAsync(settings.Login, password);
+				await smtpClient.SendAsync(message);
+				await smtpClient.DisconnectAsync(true);
+			}
 		}
 	}
 }
