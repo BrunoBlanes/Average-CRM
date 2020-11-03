ff69b6cd86e5a4d8b7981db9fd1a68598926240f
Bruno Blanes <bruno.blanes@outlook.com>
2020-11-03 19:37:19 -0300

    Code cleanup

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
