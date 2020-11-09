eaa5954ba54d00f95c948e1008a7ce6dcf926c8c
Bruno Blanes <bruno.blanes@outlook.com>
2020-11-09 12:56:00 -0300

    Resolves #87

--- Server/wwwroot/script/jQuery/Plugins/jquery.mask.js
+++ Server/wwwroot/script/jQuery/Plugins/jquery.mask.js
@@ -1,41 +1,4 @@
-﻿/**
- * jquery.mask.js
- * @version: v1.14.16
- * @author: Igor Escobar
- *
- * Created by Igor Escobar on 2012-03-10. Please report any bug at github.com/igorescobar/jQuery-Mask-Plugin
- *
- * Copyright (c) 2012 Igor Escobar http://igorescobar.com
- *
- * The MIT License (http://www.opensource.org/licenses/mit-license.php)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/* jshint laxbreak: true */
-/* jshint maxcomplexity:17 */
-/* global define */
-
-// UMD (Universal Module Definition) patterns for JavaScript modules that work everywhere.
+﻿// UMD (Universal Module Definition) patterns for JavaScript modules that work everywhere.
 // https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js
 (function (factory, jQuery, Zepto) {
 
@@ -172,7 +135,9 @@
 				el.off(['input', 'keydown', 'keyup', 'paste', 'drop', 'blur', 'focusout', ''].join('.mask '));
 			},
 			val: function(v) {
-				var isInput = el.is('fluent-text-field'),
+				var isInput = el.is('fluent-text-field')
+					|| el.is('fast-text-field')
+					|| el.is('input'),
 					method = isInput ? 'val' : 'text',
 					r;
 
@@ -467,7 +432,9 @@
 			}
 		};
 
-		jMask.init(!el.is('fluent-text-field'));
+		jMask.init(!el.is('fluent-text-field')
+			&& !el.is('fast-text-field')
+			&& !el.is('input'));
 	};
 
 	$.maskWatchers = {};
@@ -569,7 +536,7 @@
 	};
 
 	var globals = {
-		maskElements: 'input,fluent-text-field,td,span,div',
+		maskElements: 'fluent-text-field,fast-text-field,input,td,span,div',
 		dataMaskAttr: '[data-mask]',
 		dataMask: true,
 		watchInterval: 300,
