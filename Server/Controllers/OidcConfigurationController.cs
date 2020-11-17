5ed542768eab8080394c9b225ac35b8dc229706d
Bruno Blanes <bruno.blanes@outlook.com>
2020-11-17 16:59:49 -0300

    Code cleanup Resolves #104

--- Server/Controllers/OidcConfigurationController.cs
+++ Server/Controllers/OidcConfigurationController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CRM.Server.Controllers
 {
+	[Produces("application/json")]
 	public class OidcConfigurationController : Controller
 	{
 		private readonly ILogger<OidcConfigurationController> logger;
@@ -19,12 +22,30 @@ namespace CRM.Server.Controllers
 			this.logger = logger;
 		}
 
+		/// <summary>
+		/// Get the configuration for the specified <paramref name="clientId"/>.
+		/// </summary>
+		/// <param name="clientId">The client id registered with this server.</param>
+		/// <response code="200">Returns the configuration for the requested <paramref name="clientId"/>.</response>
+		/// <response code="404">If no configuration for the specified <paramref name="clientId"/> is found.</response> 
+		/// <returns></returns>
 		[HttpGet("_configuration/{clientId}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public IActionResult GetClientRequestParameters([FromRoute] string clientId)
 		{
-			IDictionary<string, string>? parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
-			logger.LogInformation($"OIDC Configuration requested by client {clientId}.");
-			return Ok(parameters);
+			try
+			{
+				IDictionary<string, string>? parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+				logger.LogInformation($"OIDC Configuration requested for {clientId}.");
+				return Ok(parameters);
+			}
+
+			catch (InvalidOperationException)
+			{
+				logger.LogWarning($"OIDC Configuration requested for {clientId} was not found.");
+				return NotFound();
+			}
 		}
 	}
 }
